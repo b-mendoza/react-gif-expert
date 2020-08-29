13629a7b3e9cc0d11c10bf9932fbe2fac6f0b017
b-mendoza <bmendoza.dev@hotmail.com>
2020-08-29 16:34:57 -0600

    Added docs

---
+++ docs/precache-manifest.4347c6e870ac2c9b0ac38b7c58457273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5a056db0ae50116dab1e754b424376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c2b9886eff316e0dc7a",
+    "url": "/static/css/main.e461e4a1.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2b9886eff316e0dc7a",
+    "url": "/static/js/main.dd644dc0.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
